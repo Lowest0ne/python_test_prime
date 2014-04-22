d192f8569333aaf119bf9f8e516c77d3ee6885fa
Carl Schwope <schwope.carl@gmail.com>
2014-04-22 16:29:31 -0400

    change range to xrange

--- prime.py
+++ prime.py
@@ -4,7 +4,7 @@ def is_prime( x ):
   if x == 2: return True
   if x < 2 or x % 2 == 0 : return False
 
-  for value in range( 3, int( sqrt(x) ) + 1, 2 ):
+  for value in xrange( 3, int( sqrt(x) ) + 1, 2 ):
     if x % value == 0: return False
 
   return True
