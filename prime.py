8f6fe404a2026c4f33ff8f2f691c869dfcf53e7d
Carl Schwope <schwope.carl@gmail.com>
2014-04-22 12:07:45 -0400

    all tests pass

--- prime.py
+++ prime.py
@@ -1,5 +1,10 @@
-def is_prime( x ):
-  if x < 2: return False
-  if x == 2: return True
-  return x % 2 == 1
+from math import sqrt
 
+def is_prime( x ):
+  if x == 2: return True
+  if x < 2 or x % 2 == 0 : return False
+
+  for value in range( 3, int( sqrt(x) ) + 1, 2 ):
+    if x % value == 0: return False
+
+  return True
