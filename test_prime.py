12e682f4043290fb2e18e2cac0691a73175f0d1b
Carl Schwope <schwope.carl@gmail.com>
2014-04-22 11:50:05 -0400

    create tests

---
+++ test_prime.py
@@ -0,0 +1,9 @@
+from prime import is_prime
+
+def test_answer():
+    assert is_prime(1) == False
+    assert is_prime(2) == True
+    assert is_prime(3) == True
+    assert is_prime(4) == False
+    assert is_prime(5) == True
+    assert is_prime(9) == False
